739fd37fa2458e2407fd7499bc73c49a9b7ddd54
feliu <enrique.navarro64@gmail.com>
2024-02-08 03:48:23 -0600

    contact route module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
