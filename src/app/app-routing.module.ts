f1647c3a290694eabf1df227bda574d3ce6fd920
ens-csv <enrique.navarro64@gmail.com>
2024-10-12 13:31:36 -0600

    Routing articles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,10 @@ import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
 	{ path: 'contact', component: ContactComponent },
-	{ path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) }];
+	{ path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) },
+    { path: '', patchMatch: 'full', redirectTo: 'articles'},
+    { path: '**', redirectTo: 'articles' }
+    ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
